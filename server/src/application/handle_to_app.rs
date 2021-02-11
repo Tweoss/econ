53480ce4b8b1796faafd2b35ebac887c5c43c280
tweoss <hwymonkey@gmail.com>
2021-02-11 14:48:19 -0800

    better docs, added some messages

--- server/src/application/handle_to_app.rs
+++ server/src/application/handle_to_app.rs
@@ -1,20 +1,43 @@
 // MESSAGES FROM HANDLERS TO THE APPLICATION
 
 use actix::prelude::*;
+// use crate::application::app::AppState;
+use crate::application::game::Game;
+
 
 /// Check if a game exists
+/// ```
+/// #[rtype(bool)]
+/// pub struct DoesGameExist {
+///     pub game_id: String,
+/// }
+/// ```
 #[derive(Message)]
 #[rtype(bool)]
 pub struct DoesGameExist {
 	pub game_id: String,
 }
-/// Check if a game exists
+
+/// Check if a game is open to players joining
+/// ```
+/// #[rtype(bool)]
+/// pub struct IsGameOpen {
+///     pub game_id: String,
+/// }
+/// ```
 #[derive(Message)]
 #[rtype(bool)]
 pub struct IsGameOpen {
 	pub game_id: String,
 }
-/// Check if a game exists
+
+/// Check if this is the Right Director Password
+/// ```
+/// #[rtype(bool)]
+/// pub struct IsRightPswd {
+///     pub pswd: String,
+/// }
+/// ```
 #[derive(Message)]
 #[rtype(bool)]
 pub struct IsRightPswd {
@@ -22,6 +45,14 @@ pub struct IsRightPswd {
 }
 
 /// Register a new player's ID in a game
+/// ```
+/// #[rtype(String)]
+/// pub struct NewPlayer {
+///     pub user_id: String,
+///     pub game_id: String,
+///     pub username: String,
+/// }
+/// ```
 #[derive(Message)]
 #[rtype(String)]
 pub struct NewPlayer {
@@ -31,6 +62,14 @@ pub struct NewPlayer {
 }
 
 /// Register ANOTHER director in a game
+/// ```
+/// #[rtype(result="()")]
+/// pub struct NewDirector {
+///     pub user_id: String,
+///     pub game_id: String,
+///     pub username: String,
+/// }
+/// ```
 #[derive(Message)]
 #[rtype(result="()")]
 pub struct NewDirector {
@@ -40,10 +79,48 @@ pub struct NewDirector {
 }
 
 /// Create a new game
+/// ```
+/// #[rtype(result="()")]
+/// pub struct NewGame {
+///     pub user_id: String,
+///     pub game_id: String,
+///     pub username: String,
+/// }
+/// ```
 #[derive(Message)]
 #[rtype(result="()")]
 pub struct NewGame {
     pub user_id: String,
     pub game_id: String,
     pub username: String,
+}
+
+/// See if a director with this ID was previously authenticated and if so, return the correct game id
+/// ```
+/// #[rtype(result="Option<Addr<Game>>")]
+/// pub struct IsDirector {
+///     pub user_id: String,
+///     pub game_id: String,
+/// }
+/// ```
+#[derive(Message)]
+#[rtype(result="Option<Addr<Game>>")]
+pub struct IsDirector {
+    pub user_id: String,
+    pub game_id: String,
+}
+
+/// See if a player with this ID was previously registered and if so, return the correct game id
+/// ```
+/// #[rtype(result="Option<Addr<Game>>")]
+/// pub struct IsPlayer {
+///     pub user_id: String,
+///     pub game_id: String,
+/// }
+/// ```
+#[derive(Message)]
+#[rtype(result="Option<Addr<Game>>")]
+pub struct IsPlayer {
+    pub user_id: String,
+    pub game_id: String,
 }
