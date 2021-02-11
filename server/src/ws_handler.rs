1a65038ff673bbd7d238dec9cf835157f0b3582d
tweoss <hwymonkey@gmail.com>
2021-02-11 15:30:15 -0800

    starting web actor server starting

--- server/src/ws_handler.rs
+++ server/src/ws_handler.rs
@@ -13,28 +13,30 @@ pub async fn handle_ws(req: HttpRequest, stream: web::Payload) -> Result<HttpRes
 
 		match viewtype.value() {
 			"director" => {
-				let resp = ws::start(MyWs::new(uuid.to_string(), game_id.to_string(), addr), &req, stream);
-				println!("{:?}", resp);
-				resp
+				if let Some(director_ws) = MyWs::new(uuid.to_string(), game_id.to_string(), addr).await {
+					let resp = ws::start(director_ws, &req, stream);
+					println!("{:?}", resp);
+					return resp;
+				}
 			}
 			"consumer" => {
-				let resp = ws::start(MyWs::new(uuid.to_string(), game_id.to_string(), addr), &req, stream);
-				println!("{:?}", resp);
-				resp
+				// let resp = ws::start(MyWs::new(uuid.to_string(), game_id.to_string(), addr), &req, stream);
+				// println!("{:?}", resp);
+				// resp;
 			}
 			"producer" => {
-				let resp = ws::start(MyWs::new(uuid.to_string(), game_id.to_string(), addr), &req, stream);
-				println!("{:?}", resp);
-				resp
+				// let resp = ws::start(MyWs::new(uuid.to_string(), game_id.to_string(), addr), &req, stream);
+				// println!("{:?}", resp);
+				// resp;
 			}
 			"viewer" => {
-				let resp = ws::start(MyWs::new(uuid.to_string(), game_id.to_string(), addr), &req, stream);
-				println!("{:?}", resp);
-				resp
+				// let resp = ws::start(MyWs::new(uuid.to_string(), game_id.to_string(), addr), &req, stream);
+				// println!("{:?}", resp);
+				// resp;
 			}
-			_ => Ok(HttpResponse::build(http::StatusCode::OK).body("Invalid Viewtype")),
+			_ => {return Ok(HttpResponse::build(http::StatusCode::OK).body("Invalid Viewtype"));},
 		}
-	} else {
-		Ok(HttpResponse::build(http::StatusCode::OK).body("No Cookie Set"))
 	}
+	Ok(HttpResponse::build(http::StatusCode::OK).body("Failed: possible reasons are no cookies set or no corresponding uuid found"))
+	
 }
