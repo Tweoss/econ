c9ad49b4b775988e5b4370d0d82b79d4cf8b3314
tweoss <hwymonkey@gmail.com>
2021-04-16 19:28:05 +0000

    Compiling server for deploy

--- server/target/x86_64-unknown-linux-gnu/release/deps/actix_learn-35e592e117940da9.d
+++ server/target/x86_64-unknown-linux-gnu/release/deps/actix_learn-35e592e117940da9.d
@@ -1,41 +1,41 @@
-/home/runner/work/econ/econ/server/target/x86_64-unknown-linux-gnu/release/deps/actix_learn-35e592e117940da9: src/main.rs src/application.rs src/application/app.rs src/application/app_to_game.rs src/application/game_folder.rs src/application/game_folder/game.rs src/application/game_folder/game_to_app.rs src/application/game_folder/game_to_consumer.rs src/application/game_folder/game_to_director.rs src/application/game_folder/game_to_participant.rs src/application/game_folder/game_to_producer.rs src/application/game_folder/game_to_viewer.rs src/application/game_folder/participants.rs src/application/game_folder/participants/consumer_folder.rs src/application/game_folder/participants/consumer_folder/consumer.rs src/application/game_folder/participants/consumer_folder/consumer_to_game.rs src/application/game_folder/participants/consumer_folder/consumer_structs.rs src/application/game_folder/participants/director_folder.rs src/application/game_folder/participants/director_folder/director_structs.rs src/application/game_folder/participants/director_folder/director_to_game.rs src/application/game_folder/participants/director_folder/director.rs src/application/game_folder/participants/participant_to_game.rs src/application/game_folder/participants/producer_folder.rs src/application/game_folder/participants/producer_folder/producer.rs src/application/game_folder/participants/producer_folder/producer_structs.rs src/application/game_folder/participants/producer_folder/producer_to_game.rs src/application/game_folder/participants/viewer_folder.rs src/application/game_folder/participants/viewer_folder/viewer.rs src/application/game_folder/participants/viewer_folder/viewer_structs.rs src/application/game_folder/participants/viewer_folder/viewer_to_game.rs src/application/game_folder/participants/heartbeat.rs src/html_handlers.rs src/ws_handler.rs src/handle_to_app.rs
+/home/runner/work/econ/econ/server/target/x86_64-unknown-linux-gnu/release/deps/actix_learn-35e592e117940da9: src/main.rs src/application.rs src/application/app.rs src/application/app_to_game.rs src/html_handlers.rs src/ws_handler.rs src/handle_to_app.rs src/game_folder.rs src/game_folder/game.rs src/game_folder/game_to_app.rs src/game_folder/game_to_consumer.rs src/game_folder/game_to_director.rs src/game_folder/game_to_participant.rs src/game_folder/game_to_producer.rs src/game_folder/game_to_viewer.rs src/participants.rs src/participants/consumer_folder.rs src/participants/consumer_folder/consumer.rs src/participants/consumer_folder/consumer_to_game.rs src/participants/consumer_folder/consumer_structs.rs src/participants/director_folder.rs src/participants/director_folder/director_structs.rs src/participants/director_folder/director_to_game.rs src/participants/director_folder/director.rs src/participants/participant_to_game.rs src/participants/producer_folder.rs src/participants/producer_folder/producer.rs src/participants/producer_folder/producer_structs.rs src/participants/producer_folder/producer_to_game.rs src/participants/viewer_folder.rs src/participants/viewer_folder/viewer.rs src/participants/viewer_folder/viewer_structs.rs src/participants/viewer_folder/viewer_to_game.rs src/participants/heartbeat.rs
 
-/home/runner/work/econ/econ/server/target/x86_64-unknown-linux-gnu/release/deps/actix_learn-35e592e117940da9.d: src/main.rs src/application.rs src/application/app.rs src/application/app_to_game.rs src/application/game_folder.rs src/application/game_folder/game.rs src/application/game_folder/game_to_app.rs src/application/game_folder/game_to_consumer.rs src/application/game_folder/game_to_director.rs src/application/game_folder/game_to_participant.rs src/application/game_folder/game_to_producer.rs src/application/game_folder/game_to_viewer.rs src/application/game_folder/participants.rs src/application/game_folder/participants/consumer_folder.rs src/application/game_folder/participants/consumer_folder/consumer.rs src/application/game_folder/participants/consumer_folder/consumer_to_game.rs src/application/game_folder/participants/consumer_folder/consumer_structs.rs src/application/game_folder/participants/director_folder.rs src/application/game_folder/participants/director_folder/director_structs.rs src/application/game_folder/participants/director_folder/director_to_game.rs src/application/game_folder/participants/director_folder/director.rs src/application/game_folder/participants/participant_to_game.rs src/application/game_folder/participants/producer_folder.rs src/application/game_folder/participants/producer_folder/producer.rs src/application/game_folder/participants/producer_folder/producer_structs.rs src/application/game_folder/participants/producer_folder/producer_to_game.rs src/application/game_folder/participants/viewer_folder.rs src/application/game_folder/participants/viewer_folder/viewer.rs src/application/game_folder/participants/viewer_folder/viewer_structs.rs src/application/game_folder/participants/viewer_folder/viewer_to_game.rs src/application/game_folder/participants/heartbeat.rs src/html_handlers.rs src/ws_handler.rs src/handle_to_app.rs
+/home/runner/work/econ/econ/server/target/x86_64-unknown-linux-gnu/release/deps/actix_learn-35e592e117940da9.d: src/main.rs src/application.rs src/application/app.rs src/application/app_to_game.rs src/html_handlers.rs src/ws_handler.rs src/handle_to_app.rs src/game_folder.rs src/game_folder/game.rs src/game_folder/game_to_app.rs src/game_folder/game_to_consumer.rs src/game_folder/game_to_director.rs src/game_folder/game_to_participant.rs src/game_folder/game_to_producer.rs src/game_folder/game_to_viewer.rs src/participants.rs src/participants/consumer_folder.rs src/participants/consumer_folder/consumer.rs src/participants/consumer_folder/consumer_to_game.rs src/participants/consumer_folder/consumer_structs.rs src/participants/director_folder.rs src/participants/director_folder/director_structs.rs src/participants/director_folder/director_to_game.rs src/participants/director_folder/director.rs src/participants/participant_to_game.rs src/participants/producer_folder.rs src/participants/producer_folder/producer.rs src/participants/producer_folder/producer_structs.rs src/participants/producer_folder/producer_to_game.rs src/participants/viewer_folder.rs src/participants/viewer_folder/viewer.rs src/participants/viewer_folder/viewer_structs.rs src/participants/viewer_folder/viewer_to_game.rs src/participants/heartbeat.rs
 
 src/main.rs:
 src/application.rs:
 src/application/app.rs:
 src/application/app_to_game.rs:
-src/application/game_folder.rs:
-src/application/game_folder/game.rs:
-src/application/game_folder/game_to_app.rs:
-src/application/game_folder/game_to_consumer.rs:
-src/application/game_folder/game_to_director.rs:
-src/application/game_folder/game_to_participant.rs:
-src/application/game_folder/game_to_producer.rs:
-src/application/game_folder/game_to_viewer.rs:
-src/application/game_folder/participants.rs:
-src/application/game_folder/participants/consumer_folder.rs:
-src/application/game_folder/participants/consumer_folder/consumer.rs:
-src/application/game_folder/participants/consumer_folder/consumer_to_game.rs:
-src/application/game_folder/participants/consumer_folder/consumer_structs.rs:
-src/application/game_folder/participants/director_folder.rs:
-src/application/game_folder/participants/director_folder/director_structs.rs:
-src/application/game_folder/participants/director_folder/director_to_game.rs:
-src/application/game_folder/participants/director_folder/director.rs:
-src/application/game_folder/participants/participant_to_game.rs:
-src/application/game_folder/participants/producer_folder.rs:
-src/application/game_folder/participants/producer_folder/producer.rs:
-src/application/game_folder/participants/producer_folder/producer_structs.rs:
-src/application/game_folder/participants/producer_folder/producer_to_game.rs:
-src/application/game_folder/participants/viewer_folder.rs:
-src/application/game_folder/participants/viewer_folder/viewer.rs:
-src/application/game_folder/participants/viewer_folder/viewer_structs.rs:
-src/application/game_folder/participants/viewer_folder/viewer_to_game.rs:
-src/application/game_folder/participants/heartbeat.rs:
 src/html_handlers.rs:
 src/ws_handler.rs:
 src/handle_to_app.rs:
+src/game_folder.rs:
+src/game_folder/game.rs:
+src/game_folder/game_to_app.rs:
+src/game_folder/game_to_consumer.rs:
+src/game_folder/game_to_director.rs:
+src/game_folder/game_to_participant.rs:
+src/game_folder/game_to_producer.rs:
+src/game_folder/game_to_viewer.rs:
+src/participants.rs:
+src/participants/consumer_folder.rs:
+src/participants/consumer_folder/consumer.rs:
+src/participants/consumer_folder/consumer_to_game.rs:
+src/participants/consumer_folder/consumer_structs.rs:
+src/participants/director_folder.rs:
+src/participants/director_folder/director_structs.rs:
+src/participants/director_folder/director_to_game.rs:
+src/participants/director_folder/director.rs:
+src/participants/participant_to_game.rs:
+src/participants/producer_folder.rs:
+src/participants/producer_folder/producer.rs:
+src/participants/producer_folder/producer_structs.rs:
+src/participants/producer_folder/producer_to_game.rs:
+src/participants/viewer_folder.rs:
+src/participants/viewer_folder/viewer.rs:
+src/participants/viewer_folder/viewer_structs.rs:
+src/participants/viewer_folder/viewer_to_game.rs:
+src/participants/heartbeat.rs:
 
 # env-dep:CARGO_PKG_NAME=actix_learn
 # env-dep:CARGO_PKG_VERSION=0.1.0
