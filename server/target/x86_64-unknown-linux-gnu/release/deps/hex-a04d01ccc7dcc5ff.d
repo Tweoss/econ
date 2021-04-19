721cc7ecaab498eb8664dcdeacb7602574de1bab
tweoss <hwymonkey@gmail.com>
2021-04-19 02:36:16 +0000

    Compiling server for deploy

---
+++ server/target/x86_64-unknown-linux-gnu/release/deps/hex-a04d01ccc7dcc5ff.d
@@ -0,0 +1,8 @@
+/home/runner/work/econ/econ/server/target/x86_64-unknown-linux-gnu/release/deps/hex-a04d01ccc7dcc5ff.rmeta: /home/runner/.cargo/registry/src/github.com-1ecc6299db9ec823/hex-0.4.3/src/lib.rs /home/runner/.cargo/registry/src/github.com-1ecc6299db9ec823/hex-0.4.3/src/error.rs
+
+/home/runner/work/econ/econ/server/target/x86_64-unknown-linux-gnu/release/deps/libhex-a04d01ccc7dcc5ff.rlib: /home/runner/.cargo/registry/src/github.com-1ecc6299db9ec823/hex-0.4.3/src/lib.rs /home/runner/.cargo/registry/src/github.com-1ecc6299db9ec823/hex-0.4.3/src/error.rs
+
+/home/runner/work/econ/econ/server/target/x86_64-unknown-linux-gnu/release/deps/hex-a04d01ccc7dcc5ff.d: /home/runner/.cargo/registry/src/github.com-1ecc6299db9ec823/hex-0.4.3/src/lib.rs /home/runner/.cargo/registry/src/github.com-1ecc6299db9ec823/hex-0.4.3/src/error.rs
+
+/home/runner/.cargo/registry/src/github.com-1ecc6299db9ec823/hex-0.4.3/src/lib.rs:
+/home/runner/.cargo/registry/src/github.com-1ecc6299db9ec823/hex-0.4.3/src/error.rs:
---
+++ server/target/x86_64-unknown-linux-gnu/release/deps/rand_core-39074858af37e22e.d
@@ -0,0 +1,12 @@
+/home/runner/work/econ/econ/server/target/x86_64-unknown-linux-gnu/release/deps/rand_core-39074858af37e22e.rmeta: /home/runner/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.6.1/src/lib.rs /home/runner/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.6.1/src/block.rs /home/runner/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.6.1/src/error.rs /home/runner/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.6.1/src/impls.rs /home/runner/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.6.1/src/le.rs /home/runner/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.6.1/src/os.rs
+
+/home/runner/work/econ/econ/server/target/x86_64-unknown-linux-gnu/release/deps/librand_core-39074858af37e22e.rlib: /home/runner/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.6.1/src/lib.rs /home/runner/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.6.1/src/block.rs /home/runner/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.6.1/src/error.rs /home/runner/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.6.1/src/impls.rs /home/runner/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.6.1/src/le.rs /home/runner/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.6.1/src/os.rs
+
+/home/runner/work/econ/econ/server/target/x86_64-unknown-linux-gnu/release/deps/rand_core-39074858af37e22e.d: /home/runner/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.6.1/src/lib.rs /home/runner/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.6.1/src/block.rs /home/runner/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.6.1/src/error.rs /home/runner/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.6.1/src/impls.rs /home/runner/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.6.1/src/le.rs /home/runner/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.6.1/src/os.rs
+
+/home/runner/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.6.1/src/lib.rs:
+/home/runner/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.6.1/src/block.rs:
+/home/runner/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.6.1/src/error.rs:
+/home/runner/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.6.1/src/impls.rs:
+/home/runner/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.6.1/src/le.rs:
+/home/runner/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.6.1/src/os.rs:
